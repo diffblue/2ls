9f8170ce618e1d3d1c9711af5be45f226136c97a
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2012-02-13 16:55:53 +0000

    two phases
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@5 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/deltacheck/collation.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************\
+
+Module: Collation
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "collation.h"
+
+/*******************************************************************\
+
+Function: collation
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void collation(const std::istream &in, const optionst &options)
+{
+}
---
+++ src/deltacheck/summarization.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************\
+
+Module: Summarization
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include "summarization.h"
+
+/*******************************************************************\
+
+Function: summarization
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void summarization(const contextt &, const goto_functionst &, const optionst &)
+{
+}
