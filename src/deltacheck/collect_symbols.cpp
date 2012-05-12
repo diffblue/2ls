b785a0d69837c93051520e2b36383cd904a7f4fd
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2012-05-12 17:12:00 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@24 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/deltacheck/collect_symbols.cpp
@@ -0,0 +1,23 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_DELTACHECK_COLLECT_SYMBOLS_H
+#define CPROVER_DELTACHECK_COLLECT_SYMBOLS_H
+
+#include <find_symbols.h>
+
+#include <goto-programs/goto_functions.h>
+
+void collect_symbols(
+  const goto_functiont &,
+  find_symbols_sett &)
+{
+
+}
+
+#endif
