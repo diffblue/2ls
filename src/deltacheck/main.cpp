624e430293c5c3b572d01f100b166630869ce126
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2012-09-18 13:52:42 +0000

    wmain
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@44 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/deltacheck/main.cpp
+++ src/deltacheck/main.cpp
@@ -6,6 +6,8 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <unicode.h>
+
 #include "parseoptions.h"
 
 /*******************************************************************\
@@ -20,8 +22,17 @@ Function: main
 
 \*******************************************************************/
 
+#ifdef _MSC_VER
+int wmain(int argc, const wchar_t **argv_wide)
+{
+  const char **argv=narrow_argv(argc, argv_wide);
+  deltacheck_parseoptionst parseoptions(argc, argv);
+  return parseoptions.main();
+}
+#else
 int main(int argc, const char **argv)
 {
   deltacheck_parseoptionst parseoptions(argc, argv);
   return parseoptions.main();
 }
+#endif
