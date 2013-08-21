6aa37001286c273771e0b70419f83e5106438520
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2013-08-21 13:56:20 +0000

    stdlib
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@248 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/deltagit/show_jobs.cpp
+++ src/deltagit/show_jobs.cpp
@@ -6,6 +6,7 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <cstdlib>
 #include <fstream>
 
 #include <util/tempfile.h>
