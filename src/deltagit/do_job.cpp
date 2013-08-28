72ec2ebd11712821e5f95d99e82a321cc2b47a9a
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2013-08-28 15:22:21 +0000

    started do_job
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@254 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/deltagit/do_job.cpp
+++ src/deltagit/do_job.cpp
@@ -6,10 +6,62 @@ Author: Daniel Kroening, kroening@kroening.com
 
 \*******************************************************************/
 
+#include <cassert>
 #include <iostream>
 
 #include "job_status.h"
 #include "do_job.h"
+
+/*******************************************************************\
+
+Function: check_out
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void check_out(job_statust &job_status)
+{
+  
+}
+
+/*******************************************************************\
+
+Function: build
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void build(job_statust &job_status)
+{
+  
+}
+
+/*******************************************************************\
+
+Function: analyse
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void analyse(job_statust &job_status)
+{
+  
+}
 
 /*******************************************************************\
 
@@ -28,10 +80,15 @@ void do_job(const std::string &id)
   // get current job status
   job_statust job_status(id);
 
-  while(job_status.status!=job_statust::DONE)
+  while(job_status.status!=job_statust::DONE &&
+        !job_status.failure)
   {
     switch(job_status.status)
     {
+    case job_statust::CHECK_OUT: check_out(job_status); break;
+    case job_statust::BUILD: build(job_status); break;
+    case job_statust::ANALYSE: analyse(job_status); break;
+    case job_statust::DONE: break;
     }
   }
 
