4c4acf54e1e3d0b0cc447a3a36929de947d4eac9
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2013-09-14 15:14:50 +0000

    logging and differential analysis
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@279 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/deltagit/do_job.cpp
+++ src/deltagit/do_job.cpp
@@ -31,13 +31,15 @@ Function: check_out
 
 void check_out(job_statust &job_status)
 {
-  std::string working_dir=job_status.id+".wd";
+  const std::string working_dir=job_status.id+".wd";
 
   std::string command;
 
-  // do a shared clone -- this uses very little disc space
+  // Do a shared clone -- this uses very little disc space.
+  // Will overwrite log.
   command="git clone --no-checkout --shared source-repo "+
-          working_dir;
+          working_dir+
+          " 2>&1 > "+job_status.id+".log";
 
   int result1=system(command.c_str());
   if(result1!=0)
@@ -47,9 +49,11 @@ void check_out(job_statust &job_status)
     return;
   }
   
-  // now do checkout; this will eat disc space
+  // Now do checkout; this will eat disc space.
   command="cd "+working_dir+"; "+
-          "git checkout --detach "+job_status.commit;
+          "git checkout --detach "+job_status.commit+
+          " 2>&1 >> "+job_status.id+".log";
+
   int result2=system(command.c_str());
 
   if(result2!=0)
@@ -77,7 +81,25 @@ Function: build
 
 void build(job_statust &job_status)
 {
-  job_status.failure=true;
+  const std::string working_dir=job_status.id+".wd";
+  
+  std::string command;
+
+  // Now run build script in working directory.
+  command="cd "+working_dir+"; ../build"+
+          " 2>&1 >> "+job_status.id+".log";
+
+  int result=system(command.c_str());
+  
+  if(result!=0)
+  {
+    job_status.failure=true;
+    job_status.write();
+    return;
+  }
+
+  job_status.status=job_statust::ANALYSE;
+  job_status.write();  
 }
 
 /*******************************************************************\
@@ -92,8 +114,31 @@ Function: analyse
 
 \*******************************************************************/
 
-void analyse(job_statust &job_status)
+void analyse(
+  job_statust &job_status,
+  const std::list<job_statust> &jobs)
 {
+  // get the job before this one
+
+  std::string previous="";
+
+  for(std::list<job_statust>::const_iterator
+      j_it=jobs.begin();
+      j_it!=jobs.end();
+      j_it++)
+  {
+    if(j_it->id==job_status.id) break;
+    previous=j_it->id;
+  }
+  
+  if(previous!="")
+    std::cout << "Differential analysis between "
+              << previous << " and " << job_status.id
+              << "\n";
+  else
+    std::cout << "One-version analysis for " << job_status.id
+              << "\n";
+
   job_status.failure=true;
 }
 
@@ -109,7 +154,8 @@ Function: do_job
 
 \*******************************************************************/
 
-void do_job(job_statust &job_status)
+void do_job(job_statust &job_status,
+            const std::list<job_statust> &jobs)
 {
   while(job_status.status!=job_statust::DONE &&
         !job_status.failure)
@@ -119,7 +165,7 @@ void do_job(job_statust &job_status)
     case job_statust::INIT: return; // done by deltagit init
     case job_statust::CHECK_OUT: check_out(job_status); break;
     case job_statust::BUILD: build(job_status); break;
-    case job_statust::ANALYSE: analyse(job_status); break;
+    case job_statust::ANALYSE: analyse(job_status, jobs); break;
     case job_statust::DONE: break;
     }
   }
@@ -140,9 +186,13 @@ Function: do_job
 
 void do_job(const std::string &id)
 {
+  // get job list
+  std::list<job_statust> jobs;
+  get_jobs(jobs);
+
   // get current job status
   job_statust job_status(id);
-  do_job(job_status);
+  do_job(job_status, jobs);
 }
 
 /*******************************************************************\
@@ -173,7 +223,7 @@ void do_job()
        !j_it->failure)
     {
       std::cout << "Job " << j_it->id << std::endl;
-      do_job(*j_it);
+      do_job(*j_it, jobs);
     }
   }
 }
