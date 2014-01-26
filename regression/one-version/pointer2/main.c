aab30e981b516f3169fb02358ebae5b358ad0bef
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-01-26 17:34:58 +0000

    added reading
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@412 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/one-version/pointer2/main.c
+++ regression/one-version/pointer2/main.c
@@ -5,9 +5,15 @@ int main()
   int *p;
   
   p=&global;
+  assert(p==&global);
   global=1;
   assert(global==1);
+  
+  // writing to pointer
   *p=2;
-  assert(p==&global);
   assert(global==2);  
+  
+  // reading from pointer
+  global=3;
+  assert(*p==3);
 }
