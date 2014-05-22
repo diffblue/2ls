c17c95b374719e278e41bad863c788f7dcc96ebd
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-05-22 13:21:03 +0000

    make malloc work
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@521 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/ssa/malloc_ssa.cpp
+++ src/ssa/malloc_ssa.cpp
@@ -9,6 +9,7 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <util/std_types.h>
 #include <util/std_expr.h>
 #include <util/arith_tools.h>
+#include <util/expr_util.h>
 #include <util/symbol.h>
 #include <util/pointer_offset_size.h>
 
@@ -107,71 +108,43 @@ exprt malloc_ssa(
         }
       }
     }
-    
+
+    // the fall-back is to produce a byte-array    
     if(object_type.is_nil())
       object_type=array_typet(unsigned_char_type(), size);
-
-    // we introduce a fresh symbol for the size
-    // to prevent any issues of the size getting ever changed
-    
-    if(object_type.id()==ID_array &&
-       !to_array_type(object_type).size().is_constant())
-    {
-      exprt &size=to_array_type(object_type).size();
-    
-      symbolt size_symbol;
-
-      size_symbol.base_name="dynamic_object_size"+suffix;
-      size_symbol.name="ssa::"+id2string(size_symbol.base_name);
-      size_symbol.is_lvalue=true;
-      size_symbol.type=size.type();
-      size_symbol.mode=ID_C;
-
-      //state.var_map(size_symbol.name, suffix, size_symbol.type);
-
-      #if 0
-      assign(state,
-             size_symbol.symbol_expr(), 
-             size);
-
-      size=size_symbol.symbol_expr();
-      #endif
-    }
   }
   
-  #if 0
   // value
   symbolt value_symbol;
 
-  value_symbol.base_name="dynamic_object"+i2string(state.var_map.dynamic_count);
-  value_symbol.name="symex_dynamic::"+id2string(value_symbol.base_name);
+  value_symbol.base_name="dynamic_object"+suffix;
+  value_symbol.name="ssa::"+id2string(value_symbol.base_name);
   value_symbol.is_lvalue=true;
   value_symbol.type=object_type;
   value_symbol.type.set("#dynamic", true);
   value_symbol.mode=ID_C;
 
-  //state.var_map(value_symbol.name, suffix, value_symbol.type);
-
-  address_of_exprt rhs;
+  address_of_exprt address_of;
   
   if(object_type.id()==ID_array)
   {
-    rhs.type()=pointer_typet(value_symbol.type.subtype());
+    address_of.type()=pointer_typet(value_symbol.type.subtype());
     index_exprt index_expr(value_symbol.type.subtype());
     index_expr.array()=value_symbol.symbol_expr();
     index_expr.index()=gen_zero(index_type());
-    rhs.op0()=index_expr;
+    address_of.op0()=index_expr;
   }
   else
   {
-    rhs.op0()=value_symbol.symbol_expr();
-    rhs.type()=pointer_typet(value_symbol.type);
+    address_of.op0()=value_symbol.symbol_expr();
+    address_of.type()=pointer_typet(value_symbol.type);
   }
   
-  if(rhs.type()!=lhs.type())
-    rhs.make_typecast(lhs.type());
+  exprt result=address_of;
+  
+  if(result.type()!=code.type())
+    result=typecast_exprt(result, code.type());
 
-  assign(state, lhs, rhs);
-  #endif
+  return result;
 }
 
