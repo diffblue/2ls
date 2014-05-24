6b9c3618fe5c474c743177cd4e11423e65bc1f05
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-05-24 12:05:44 +0000

    SSA simplifier
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@528 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/ssa/simplify_ssa.cpp
@@ -0,0 +1,27 @@
+/*******************************************************************\
+
+Module: SSA Simplification
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <util/simplify_expr.h>
+
+#include "simplify_ssa.h"
+
+/*******************************************************************\
+
+Function: simplify_ssa
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void simplify(local_SSAt &)
+{
+}
---
+++ src/ssa/simplify_ssa.h
@@ -0,0 +1,16 @@
+/*******************************************************************\
+
+Module: SSA Simplification
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_SIMPLIFY_SSA_H
+#define CPROVER_SIMPLIFY_SSA_H
+
+#include "local_ssa.h"
+
+void simplify(local_SSAt &);
+
+#endif
