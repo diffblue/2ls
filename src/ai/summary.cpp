b35cf1c887aa45916d3f4de45ab3a69cdd568284
schram <schram@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-05-29 13:25:57 +0000

    missing file
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/summary@547 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/ai/summary.cpp
@@ -0,0 +1,18 @@
+#include "summary.h"
+#include <langapi/language_util.h>
+
+void summaryt::output(std::ostream &out, const namespacet &ns) const
+{
+  out << "entry vars: ";
+  for(summaryt::var_listt::const_iterator it = entry_vars.begin();
+      it != entry_vars.end(); it++)
+    out << from_expr(ns,"",*it) << " ";
+  out << std::endl;
+  out << "exit vars: ";
+  for(summaryt::var_listt::const_iterator it = exit_vars.begin();
+      it != exit_vars.end(); it++)
+    out << from_expr(ns,"",*it) << " ";
+  out << std::endl;
+  out << "precondition: " << from_expr(ns,"",precondition) << std::endl;
+  out << "transformer: " << from_expr(ns,"",transformer) << std::endl;
+}
