42771568e1e60958264fb89e735b8cfed49e2bfd
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-06-08 08:27:11 +0000

    more
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@658 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/one-version/loop3/main.c
+++ regression/one-version/loop3/main.c
@@ -1,4 +1,4 @@
-int x, y;
+int x, y, z;
 
 int main()
 {
@@ -10,6 +10,12 @@ int main()
   y=0;
   do y++; while(y!=100);
   assert(y==100);
+  
+  // Same again with a 'break', but this is basically
+  // the same CFG as the first one.
+  z=0;
+  while(1) { if(z==100) break; z++; }
+  assert(z==100);
   
   return 0;
 }
