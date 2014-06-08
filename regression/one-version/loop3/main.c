4cb6719335c7081d1cd0d4e11449f88d7aca1331
kroening <kroening@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-06-08 08:07:37 +0000

    check do-while guards
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/trunk@657 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/one-version/loop3/main.c
+++ regression/one-version/loop3/main.c
@@ -1,11 +1,15 @@
+int x, y;
+
 int main()
 {
-  int x;
-  
   // does not require invariant, just exit condition
   for(x=0; x!=100; x++);
-  
   assert(x==100);
+  
+  // same again with do-while
+  y=0;
+  do y++; while(y!=100);
+  assert(y==100);
   
   return 0;
 }
