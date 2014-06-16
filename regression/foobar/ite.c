dcd99c1b1488af8fe3ec7e42e2e40587d76267a7
bjowac <bjowac@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-06-16 13:22:16 +0000

    loop2
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/summary@723 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/foobar/ite.c
+++ regression/foobar/ite.c
@@ -18,3 +18,35 @@ void doit(int x)
   x++;
   assert(foo(x,y)<=z);
 }
+
+void entry()
+{
+
+  int x;
+  int y;
+  
+  if(x>=5)
+  {
+    y=x;
+  } else
+  {
+    y=5;
+  }
+  
+  y=y;
+  
+  assert(y>=5);
+}
+
+function to be analyzed: 
+(E) $cond#31 == TRUE
+(E) y#30 == x#27
+(E) $guard#30 == ($guard#27 && !$cond#29)
+(E) $guard#27 == TRUE
+(E) $cond#29 == !(x#27 >= 5)
+(E) y#32 == 5
+(E) $guard#32 == !(x#27 >= 5) 
+(E) y#phi33 == ($guard#32 ? y#32 : y#30)
+(E) y#33 == y#phi33
+(E) $guard#33 == ($guard#30 && $cond#31 || $guard#32 && TRUE)
+
---
+++ regression/foobar/loop2.c
@@ -0,0 +1,17 @@
+void doit()
+{
+
+  int x;
+  int y;
+  
+  x=0;
+  y=0;
+  
+  while(x<10)
+  {
+    ++x;
+    ++y;
+  }
+  
+  int z=x+y;
+}
