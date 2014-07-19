84d939d8d360c717be9b9cb39a4791ad18741675
schram <schram@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-07-19 12:50:23 +0000

    added a2ps benchmarks
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/summary@905 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ regression/debian/runall.sh
@@ -0,0 +1,64 @@
+SUMMARIZER=../../../src/summarizer/summarizer
+#CHECKS="--div-by-zero-check --signed-overflow-check --array-abstraction"
+CHECKS="--div-by-zero-check --signed-overflow-check --bounds-check --pointer-check"
+SETS="a2ps" 
+
+TIMEOUT=900
+
+for o in havoc intervals equalities zones
+do
+  for d in $SETS
+  do
+    cd $d
+    rm -f ../$d.$o.log
+    for f in *.o 
+    do 
+      echo $d/$f "using" $o
+      echo "FILE:" $f >> ../$d.$o.log
+      (time (perl -e 'alarm shift @ARGV; exec @ARGV' $TIMEOUT $SUMMARIZER $CHECKS $f --$o)) &>> ../$d.$o.log
+    done
+    cd ..
+  done
+done
+
+for o in intervals equalities zones
+do
+  for d in $SETS
+  do
+    cd $d
+    rm -f ../$d.context_sensitive_$o.log
+    for f in *.o 
+    do 
+      echo $d/$f "using context-sensitive" $o
+      echo "FILE:" $f >> ../$d.context_sensitive_$o.log
+      (time (perl -e 'alarm shift @ARGV; exec @ARGV' $TIMEOUT $SUMMARIZER $CHECKS $f --context-sensitive --$o)) &>> ../$d.context_sensitive_$o.log
+    done
+    cd ..
+  done
+done
+
+for d in $SETS 
+do
+  cd $d
+  rm -f ../$d.unwind5.log
+  for f in *.o 
+  do 
+    echo $d/$f "using unwind5"
+    echo "FILE:" $f >> ../$d.unwind5.log
+    (time (perl -e 'alarm shift @ARGV; exec @ARGV' $TIMEOUT $SUMMARIZER $CHECKS $f --havoc --inline --unwind 5)) &>> ../$d.unwind5.log
+  done
+  cd ..
+done
+
+for d in $SETS 
+do
+  cd $d
+  rm -f ../$d.unwind10.log
+  for f in *.o 
+  do 
+    echo $d/$f "using unwind10"
+    echo "FILE:" $f >> ../$d.unwind10.log
+    (time (perl -e 'alarm shift @ARGV; exec @ARGV' $TIMEOUT $SUMMARIZER $CHECKS $f --havoc --inline --unwind 10)) &>> ../$d.unwind10.log
+  done
+  cd ..
+done
