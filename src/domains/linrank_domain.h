cdb5124c090fae7edd11177a2968d84901ae9400
cdavid <cdavid@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-09-02 10:54:25 +0000

    added the interface for the linrank domain
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/termination@946 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/domains/linrank_domain.h
+++ src/domains/linrank_domain.h
@@ -13,7 +13,17 @@ class linrank_domaint : public domaint
 public:
   typedef unsigned rowt;
   typedef exprt row_exprt; 
-  typedef constant_exprt row_valuet; // "bound"
+  typedef struct
+  {
+    std::vector<exprt> c;
+    exprt d;
+  } row_valuet;
+
+  /*  typedef struct
+  {
+    std::vector<symbol_exprt> c;
+    symbol_exprt d;
+    } row_symb_valuet; */
 
   class templ_valuet : public domaint::valuet, public std::vector<row_valuet> 
   {
@@ -28,6 +38,8 @@ public:
   } template_rowt;
 
   typedef std::vector<template_rowt> templatet;
+  typedef std::vector<std::pairt<exprt,exprt> > pre_post_valuest;
+
 
   linrank_domaint(replace_mapt &_renaming_map, 
                    templatet &_template) :
@@ -39,40 +51,18 @@ public:
   virtual void initialize(valuet &value);
 
   // value -> constraints
-  exprt get_row_constraint(const rowt &row, const row_valuet &row_value);
-  exprt get_row_pre_constraint(const rowt &row, const row_valuet &row_value);
-  exprt get_row_post_constraint(const rowt &row, const row_valuet &row_value);
-  exprt get_row_pre_constraint(const rowt &row, const templ_valuet &value);
-  exprt get_row_post_constraint(const rowt &row, const templ_valuet &value);
-
-  exprt to_pre_constraints(const templ_valuet &value);
-  void make_not_post_constraints(const templ_valuet &value,
-			   exprt::operandst &cond_exprs, 
-			   exprt::operandst &value_exprs);
-
-  // value -> symbolic bound constraints (for optimization)
-  exprt to_symb_pre_constraints(const templ_valuet &value);
-  exprt to_symb_pre_constraints(const templ_valuet &value,
-				const std::set<rowt> &symb_rows);
-  exprt to_symb_post_constraints();
-  exprt get_row_symb_value_constraint(const rowt &row, 
-				      const row_valuet &row_value);
-  exprt get_row_symb_pre_constraint(const rowt &row, 
-				      const row_valuet &row_value);
-  exprt get_row_symb_post_constraint(const rowt &row);
+  exprt get_not_constraints(const templ_valuet &value,
+			    exprt::operandst &cond_exprs,// identical to before 
+			    pre_post_valuest &value_exprs); // (x, x')
+  exprt get_row_symb_contraint(row_valuet &symb_values, // contains vars c and d
+			       const rowt &row,
+			       pre_post_valuest &values
+			       );
 
 
   // set, get value
   row_valuet get_row_value(const rowt &row, const templ_valuet &value);
   void set_row_value(const rowt &row, const row_valuet &row_value, templ_valuet &value);
-
-  // max, min, comparison
-  row_valuet get_max_row_value(const rowt &row);
-  row_valuet get_min_row_value(const rowt &row);
-  row_valuet between(const row_valuet &lower, const row_valuet &upper);
-  bool less_than(const row_valuet &v1, const row_valuet &v2);
-  bool is_row_value_inf(const row_valuet & row_value) const;
-  bool is_row_value_neginf(const row_valuet & row_value) const;
 
   // printing
   virtual void output_value(std::ostream &out, const valuet &value, const namespacet &ns) const;
@@ -86,31 +76,15 @@ public:
   unsigned template_size();
 
   // generating templates
-  static void add_interval_template(templatet &templ,
+  static void add_template(templatet &templ,
 			      const var_specst &var_specs,
 			      const namespacet &ns);
-  static void add_zone_template(templatet &templ,
-				 const var_specst &var_specs,
-				 const namespacet &ns);
-  static void add_octagon_template(templatet &templ,
-				    const var_specst &var_specs,
-				    const namespacet &ns);
-
-  exprt get_row_symb_value(const rowt &row);
 
 protected:
-  friend class strategy_solver_binsearcht;
-
   templatet &templ;
 
   
 };
-
-void extend_expr_types(exprt &expr);
-constant_exprt simplify_const(const exprt &expr);
-ieee_floatt simplify_const_float(const exprt &expr);
-mp_integer simplify_const_int(const exprt &expr);
-
 
 
 #endif
