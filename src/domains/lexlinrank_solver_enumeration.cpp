878d6ea99231b282b99a18877437da93930b8bf3
cdavid <cdavid@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-09-30 14:32:58 +0000

    fix lexlinsolver (still getting UNSAT from the inner solver for sat instances)
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/termination@1068 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/domains/lexlinrank_solver_enumeration.cpp
+++ src/domains/lexlinrank_solver_enumeration.cpp
@@ -2,10 +2,11 @@
 
 #include <util/simplify_expr.h>
 #include "lexlinrank_solver_enumeration.h"
+#include "util.h"
 
-#define DEBUG_FORMULA 
+//#define DEBUG_FORMULA 
 #define MAX_ELEMENTS 2 // lexicographic components
-#define MAX_OUTER_ITERATIONS 50
+#define MAX_REFINEMENTS 50
 
 
 bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
@@ -17,8 +18,8 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
   static std::vector<int> number_elements_per_row;
   number_elements_per_row.resize(rank.size());
 
-  //static int number_outer_iterations;
-  //number_outer_iterations = 0;
+  //static int number_refinements;
+  //number_refinements = 0;
 
   debug() << "(RANK) no rows = " << rank.size() << eom;
 
@@ -48,8 +49,6 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
   }
 #endif
 
-
-
   rank_cond_literals.resize(rank_cond_exprs.size());
   
   for(unsigned i = 0; i < rank_cond_exprs.size(); i++)
@@ -68,7 +67,7 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
 
   if(solve() == decision_proceduret::D_SATISFIABLE) 
     { 
-      debug() << "SAT" << eom;
+      debug() << "(RANK) outer solver: SAT" << eom;
 
   
     for(unsigned row = 0; row < rank_cond_literals.size(); row++)
@@ -96,7 +95,7 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
 	lexlinrank_domaint::row_valuet symb_values;
         symb_values.resize(rank[row].size());
 
-	debug() << "elements: " << rank[row].size() << eom;
+	//debug() << "elements: " << rank[row].size() << eom;
 
 	exprt constraint;
 	exprt refinement_constraint;
@@ -105,12 +104,15 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
 	constraint = lexlinrank_domain.get_row_symb_constraint(symb_values, row, values, refinement_constraint);
 
 	simplify_expr(constraint, ns);
-	debug() << "Inner Solver: " << row << " constraint " 
-		    << from_expr(ns,"", constraint) << eom;
+	debug() << "(RANK) Constraint sent to the inner Solver: " << row << " constraint ";
+
+	pretty_print_termination_argument(debug(), ns, constraint);
+
+	debug() << eom;
 
 	inner_solver << constraint;
 
-	debug() << "inner solve()" << eom;
+	debug() << "(RANK) inner solve()" << eom;
 
         //set assumptions for refinement
         bvt assumptions;
@@ -121,19 +123,21 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
           assumptions[0] = inner_solver.convert(refinement_constraint);
 	}	
 
-	//debug() << "LALALA" << eom;
         inner_solver.set_assumptions(assumptions);
 
 	// solve
-	if(inner_solver() == decision_proceduret::D_SATISFIABLE && 
-	   number_outer_iterations < MAX_OUTER_ITERATIONS) 
+	//decision_proceduret::
+	bool inner_solver_result = inner_solver(); 
+	if(inner_solver_result == decision_proceduret::D_SATISFIABLE && 
+	   number_refinements < MAX_REFINEMENTS) 
 	{ 
 
-	  number_outer_iterations++;
+	  number_refinements++;
 	  
-	  debug() << "(RANK) inner solver: number of outer refinements = " << number_outer_iterations << eom;
-	  debug() << "(RANK) inner solver: number of components for row " << row << " is " << number_elements_per_row[row] << eom;
-	  debug() << "(RANK) inner solver: SAT" << eom;
+	  debug() << "(RANK) inner solver: SAT and the max number of refinements was not reached " << eom;
+	  debug() << "(RANK) inner solver: Current number of refinements = " << number_refinements << eom;
+	  debug() << "(RANK) inner solver: Current number of components for row " << row << " is " << number_elements_per_row[row] << eom;
+
 
 	  // new_row_values will contain the new values for c and d
 	  lexlinrank_domaint::row_valuet new_row_values;
@@ -149,9 +153,11 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
 	      {
 		exprt v = inner_solver.get(*it);
 		new_row_values[constraint_no].c.push_back(v);
-		debug() << "Inner Solver: " << row << " c value for " 
-			<< from_expr(ns,"", *it) << ": " 
-			<< from_expr(ns,"", v)  << eom;
+		debug() << "Inner Solver: " << row << " c value for ";
+		pretty_print_termination_argument(debug(), ns, *it); 
+		debug() << ": "; 
+		pretty_print_termination_argument(debug(), ns, v);
+		debug() << eom;
 	      }
 
 	    // get the model for d
@@ -169,19 +175,24 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
 
 	}
 	else {
-	  debug() << "(RANK) inner solver: UNSAT or reached max number of outer refinements" << eom;
+	  if(inner_solver_result == decision_proceduret::D_UNSATISFIABLE)
+	    debug() << "(RANK) inner solver: UNSAT" << eom;
+	  else
+	    debug() << "(RANK) inner solver: reached max number of refinements" << eom;
+
+	  debug() << "(RANK) inner solver: number of refinements = " << number_refinements << eom;
 
 	  if(number_elements_per_row[row] == MAX_ELEMENTS-1) {
-	    debug() << "(RANK) reached the max no of refinements and no ranking function was found" << eom;
+	    debug() << "(RANK) reached the max no of lexicographic components and no ranking function was found" << eom;
 	    // no ranking function for the current template
 	    lexlinrank_domain.set_row_value_to_true(row, rank);
 	  }
 	  else {
-	    debug() << "(RANK) inner solver: reached max number of outer refinements" << eom;
-	    lexlinrank_domain.add_element(row, rank);
-	    number_outer_iterations = 0;
 	    number_elements_per_row[row]++;
-	    debug() << "(RANK) the no of refinements for row " << row << " was increased to " << number_elements_per_row[row] << eom;
+	    debug() << "(RANK) inner solver: increasing the number of lexicographic componenets for row " << row << " to " << number_elements_per_row[row] << eom;
+	    lexlinrank_domain.add_element(row, rank);
+	    number_refinements = 0;
+	    debug() << "(RANK) inner solver: the number of refinements for row " << row << " was reset to " << number_refinements << eom;
 	    improved = true;
 	  }
 	}
@@ -191,7 +202,7 @@ bool lexlinrank_solver_enumerationt::iterate(invariantt &_rank)
   }
   else 
     {
-      debug() << "(RANK) outer solver: UNSAT" << eom;
+      debug() << "(RANK) outer solver: UNSAT!!" << eom;
     }
 
   pop_context();
