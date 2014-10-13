8297c2435eed1e2e17606ee8b9174eba5e045095
bjowac <bjowac@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-10-13 16:15:22 +0000

    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/termination@1139 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ regression/termination/running/main.c
@@ -0,0 +1,32 @@
+/* 
+  C version of the lapack library
+
+  http://www.netlib.org/clapack/cblas/sasum.c
+*/
+
+int nondet_int();
+
+int f(int n, int incx)
+{
+  int  nincx = n * incx;
+  int i;
+  //for (i=1; i <= nincx; i += incx); 
+  for (i=1; incx < 0 ? i >= nincx : i <= nincx; i += incx);
+}
+
+int main()
+{
+
+  int n =nondet_int(), incx=nondet_int();
+  
+  __CPROVER_assume(incx >= 0 && incx < 1000);
+  __CPROVER_assume(n < 1000 && n > 0);
+
+/*
+  int n=10;
+  int incx=20;
+ */
+	f(n, incx);
+	return 0;
+
+}
