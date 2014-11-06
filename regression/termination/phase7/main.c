6fee17277df35ebe00bc8fb6a63ef3f567b8cfac
schram <schram@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-11-06 23:58:38 +0000

    added lexicographic ranking function example
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/termination@1182 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/termination/phase7/main.c
+++ regression/termination/phase7/main.c
@@ -8,11 +8,9 @@ void main()
 
   while(x>0 && u < INT_MAX) //-u, -y, x
   {
-    //__CPROVER_assume(u<INT_MAX);
     if(y<10) x++;
     else x--;
     if(y==INT_MAX) u++;
     y++;
   }
-  //assert(x==0);
 }
--- regression/termination/phase8/main.c
+++ regression/termination/phase8/main.c
@@ -1,24 +1,13 @@
-#include <limits.h>
-
 void main()
 {
   int x = 1;  
   int y = 1;  
-  int u = 0;
-  int w = 0;
 
-  while(x>0 && w<INT_MAX) //-u, -y, x
+  while(x>0)
   {
-    //__CPROVER_assume(u<INT_MAX);
-    if(y<10) x++;
+    if(y<10) x = nondet();
     else x--;
-    if(y==INT_MAX) {
-      if (u==INT_MAX) {
-	w++;
-      }
-      u++;
-    }
-    y++;
+    if(y<100) y++;
   }
   assert(x==0);
 }
