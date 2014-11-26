5ef9780d4e1579151d8db86cb130f19c32192b51
joshi <joshi@ae12b94c-7a1e-481b-bb16-7d291d499974>
2014-11-26 14:49:25 +0000

    main changed
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/summary@1208 ae12b94c-7a1e-481b-bb16-7d291d499974

--- regression/summarizer/nested12/main.c
+++ regression/summarizer/nested12/main.c
@@ -3,14 +3,17 @@
 extern int nondet_int();
 
 int main() { 
-  int i=0, n=3;
+  int i=0, n=3,sn=0,x,y;
 
-  int sn = nondet_int();
+  for(x=0;x<5;x++)
+  {
+   sn = nondet_int();
   
   while(i<n) {
     sn = sn + a;
     i++;
   }
 
+  }
   assert(sn== n*a || sn == 0);
 }
