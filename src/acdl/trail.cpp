914420b77b4c6a8c1b27b248904a3a2063807a12
rajdeep <rajdeep@ae12b94c-7a1e-481b-bb16-7d291d499974>
2015-12-11 13:44:25 +0000

    trail header
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/acdl@1549 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/acdl/trail.cpp
+++ src/acdl/trail.cpp
@@ -1,7 +1,6 @@
 /*******************************************************************\
 
-Module: Trail data structure used for backtracking. It stores 
-assignment history and allows retrieval of past variable values
+Module: Trail datastructure used for backtracking. It stores assignment history and allows retrieval of past variable values
 
 Author: Rajdeep Mukherjee, Peter Schrammel
 
@@ -23,6 +22,33 @@ public:
     {
     }
 
-void assign(const acdl_domaint& );
+  void assign(const acdl_domaint& );
 
- 
+  // backtrack one deduction
+  void backtrack_once();
+  
+  // backtrack to just before trail index
+  void backtrack_to_idx(unsigned idx);
+
+  // backtrack to decision level
+  void backtrack_to_lvl(unsigned idx);
+
+  const acdl_domaint::valuet& dlevel0_values(){
+    return values_dlevel0;
+  }
+
+  // returns the value of a variable before trail index idx
+  acdl_domaint::valuet value_before(
+    unsigned var, unsigned idx);
+  
+ unsigned dlevel() { return control.size(); }
+
+protected: 
+  acdl_domaint& dom;
+  acdl_domaint::valuet& values;
+  acdl_domaint::valuet values_dlevel0;
+
+  std::vector<int> control;
+  std::vector<int> dlevels;
+  std::vector<acdl_domaint::valuet> prev_values;
+};
