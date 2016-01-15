2b9c7c16bf526a2e6a7ea3c182c8616d54e4e7d4
schram <schram@ae12b94c-7a1e-481b-bb16-7d291d499974>
2016-01-15 10:50:22 +0000

    refactoring of worklist class
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/acdl@1592 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/acdl/acdl_worklist_base.cpp
@@ -0,0 +1,203 @@
+/*******************************************************************\
+
+Module: ACDL Worklist Management Base
+
+Author: Rajdeep Mukherjee, Peter Schrammel
+
+ \*******************************************************************/
+
+#include "acdl_worklist_base.h"
+
+/*******************************************************************	\
+
+Function: acdl_worklist_baset::check_statement()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+
+bool
+acdl_worklist_baset::check_statement (const exprt &expr,
+                               const acdl_domaint::varst &vars)
+{
+
+  std::set<symbol_exprt> symbols;
+  // find all variables in a statement
+  find_symbols (expr, symbols);
+  // check if vars appears in the symbols set,
+  // if there is a non-empty intersection, then insert the
+  // equality statement in the worklist
+  for (acdl_domaint::varst::const_iterator it = vars.begin ();
+      it != vars.end (); it++)
+  {
+    if (symbols.find (*it) != symbols.end ())
+    {
+      // find_symbols here may be required to 
+      // find transitive dependencies
+      // in which case make vars non-constant
+      //find_symbols(expr, vars);
+      return true;
+    }
+  }
+  return false;
+}
+
+/*******************************************************************\
+
+Function: acdl_worklist_baset::push_into_worklist()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+
+void
+acdl_worklist_baset::push (const acdl_domaint::statementt &statement)
+{
+#if 1 // list implementation
+  for(worklistt::const_iterator it = worklist.begin();
+      it != worklist.end(); ++it)
+    if(statement == *it)
+      return;
+  worklist.push_back(statement);
+#else // set implementation
+  worklist.insert(statement);
+#endif
+}
+
+/*******************************************************************\
+
+Function: acdl_worklist_baset::pop_from_worklist()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+
+const acdl_domaint::statementt
+acdl_worklist_baset::pop ()
+{
+#if 1
+  const acdl_domaint::statementt statement = worklist.front();
+  worklist.pop_front();
+#else
+  worklistt::iterator it = worklist.begin ();
+  const exprt statement = *it;
+  worklist.erase (it);
+#endif
+  return statement;
+}
+
+/*******************************************************************\
+
+Function: acdl_worklist_baset::update_worklist()
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+
+
+void
+acdl_worklist_baset::update (const local_SSAt &SSA,
+                               const acdl_domaint::varst &vars,
+                               const acdl_domaint::statementt &current_statement)
+{
+  // dependency analysis loop for equalities
+  for (local_SSAt::nodest::const_iterator n_it = SSA.nodes.begin ();
+      n_it != SSA.nodes.end (); n_it++)
+  {
+
+    for (local_SSAt::nodet::equalitiest::const_iterator e_it =
+        n_it->equalities.begin (); e_it != n_it->equalities.end (); e_it++)
+    {
+      // the statement has already been processed, so no action needed
+      if(*e_it == current_statement) continue;
+
+      if (check_statement (*e_it, vars)) {
+        push_into_worklist(worklist, *e_it);
+        #ifdef DEBUG
+        std::cout << "Push: " << from_expr (SSA.ns, "", *e_it) << std::endl;
+        #endif
+      }
+    }
+    for (local_SSAt::nodet::constraintst::const_iterator c_it =
+        n_it->constraints.begin (); c_it != n_it->constraints.end (); c_it++)
+    {
+      if(*c_it == current_statement) continue;
+      if (check_statement (*c_it, vars)) {
+        push_into_worklist(worklist, *c_it);
+        #ifdef DEBUG
+        std::cout << "Push: " << from_expr (SSA.ns, "", *c_it) << std::endl;
+        #endif
+      }
+    }
+    for (local_SSAt::nodet::assertionst::const_iterator a_it =
+        n_it->assertions.begin (); a_it != n_it->assertions.end (); a_it++)
+    {
+      // for now, store the decision variable as variables 
+      // that appear only in properties
+      // find all variables in an assert statement
+      assert_listt alist;
+      push_into_assertion_list(alist, *a_it);
+           
+      if(*a_it == current_statement) continue;
+      if (check_statement (*a_it, vars)) {
+        push_into_worklist(worklist, not_exprt (*a_it));
+        #ifdef DEBUG
+        std::cout << "Push: " << from_expr (SSA.ns, "", not_exprt(*a_it)) << std::endl;
+        #endif
+      }
+    }
+  }
+}
+
+
+/*******************************************************************
+
+ Function: acdl_worklist_baset::select_vars()
+
+ Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+
+void
+acdl_worklist_baset::select_vars (const exprt &statement, acdl_domaint::varst &vars)
+{
+#if 0 //TODO: this was an attempt to implement a forward iteration strategy,
+      //      but we would also need to consider execution order 
+  // If it is an equality, then select the lhs for post-condition computation
+  exprt lhs;
+  if (statement.id () == ID_equal)
+  {
+    lhs = to_equal_expr (statement).lhs ();
+    if (lhs.id () == ID_symbol)
+    {
+      vars.insert (to_symbol_expr (lhs));
+    }
+    else //TODO: more complex lhs
+      assert(false);
+  }
+  else // for constraints
+#endif
+  {
+    find_symbols(statement,vars);
+  }
+}
