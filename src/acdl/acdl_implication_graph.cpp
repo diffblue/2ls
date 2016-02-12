0278c7e053de73160141d75ab21175cbb642000c
rajdeep <rajdeep@ae12b94c-7a1e-481b-bb16-7d291d499974>
2016-02-12 13:49:29 +0000

    acdl implication graph
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/acdl@1603 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ src/acdl/acdl_implication_graph.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************\
+
+Module: Implication graph implementation
+
+Author: Rajdeep Mukherjee, Peter Schrammel
+
+\*******************************************************************/
+
+#include "acdl_implication_graph.h"
+
+/*******************************************************************\
+
+Function: acdl_conflict_analysis_baset::
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+void acdl_implication_grapht::add_deductions
+  (const acdl_domaint::deductionst &m_ir) {
+ 
+  //g.current_level = 0;
+}
+
+/*******************************************************************\
+
+Function: acdl_conflict_analysis_baset::
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+ \*******************************************************************/
+void acdl_implication_grapht::add_decision
+  (const acdl_domaint::meet_irreduciblet & m_ir) {
+  
+  //g.current_level = 1;
+}
+  
+
