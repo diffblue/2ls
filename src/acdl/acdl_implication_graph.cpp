d5f6e68a8b97afe548bf22cb680781e7ed7a58a2
rajdeep <rajdeep@ae12b94c-7a1e-481b-bb16-7d291d499974>
2016-02-13 14:41:02 +0000

    decision and deduction node created
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/acdl@1610 ae12b94c-7a1e-481b-bb16-7d291d499974

--- src/acdl/acdl_implication_graph.cpp
+++ src/acdl/acdl_implication_graph.cpp
@@ -22,7 +22,31 @@ Function: acdl_implication_grapht::add_deductions
 void acdl_implication_grapht::add_deductions
   (const acdl_domaint::deductionst &m_ir)
 {
-  current_level = 0;
+  nodest deduce_node;
+  // create a node for each new meet irreducibles
+  for(std::vector<acdl_domaint::deductiont>::const_iterator it 
+    = m_ir.begin(); it != m_ir.end(); it++)
+  {  
+    acdl_implication_graph_nodet node;
+    node.expr = it->first;
+    node.is_decision = false;
+    node.level = current_level + 1;
+    current_level = current_level + 1;
+    acdl_domaint::antecedentst ant = it->second;
+    deduce_node.push_back(node);
+    
+    // create a node for each antecedents which are 
+    // used to infer the new meet irreducible
+    for(std::vector<acdl_domaint::meet_irreduciblet>::const_iterator 
+      it1 = ant.begin(); it1 != ant.end(); it1++) {
+      acdl_implication_graph_nodet ant_node;
+      ant_node.expr = *it1;
+      ant_node.is_decision = false;
+      ant_node.level = current_level + 1;
+      current_level = current_level + 1;
+      deduce_node.push_back(ant_node);
+    }
+  }
 }
 
 /*******************************************************************\
@@ -39,7 +63,13 @@ Function: acdl_implication_grapht::add_decision
 void acdl_implication_grapht::add_decision
   (const acdl_domaint::meet_irreduciblet & m_ir)
 {
-  current_level = 1;
+  acdl_implication_graph_nodet node;
+  node.expr = m_ir;
+  node.is_decision = true;
+  node.level = current_level+1;
+  current_level = current_level + 1;
+  nodest decide_node;
+  decide_node.push_back(node);
 }
  
 
