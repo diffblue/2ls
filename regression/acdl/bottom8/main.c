abc2e760728d27527a1c04ee5b0fee3897797b80
rajdeep <rajdeep@ae12b94c-7a1e-481b-bb16-7d291d499974>
2016-04-02 08:51:51 +0000

    regresssion added
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/software/deltacheck/branches/acdl@1658 ae12b94c-7a1e-481b-bb16-7d291d499974

---
+++ regression/acdl/bottom8/main.c
@@ -0,0 +1,6 @@
+void main() {
+  int x, y;
+  x = 2 * y;
+  x = x + 1;
+  assert(x != 4);
+}
---
+++ regression/acdl/if-else2/main.c
@@ -0,0 +1,9 @@
+void main()
+{
+ int x;
+ if(x==0 || x==1)
+ {
+   x = x * 2; 
+   assert(x != 1);
+ }
+}
