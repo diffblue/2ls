511ca58658954fa4c9b56a44a6e61f43f969a030
Stefan <steffan711@gmail.com>
2016-12-08 15:06:02 +0100

    first commit

---
+++ src/ssa/ssa_dereference.d
@@ -0,0 +1,141 @@
+ssa_dereference.o: ssa_dereference.cpp \
+ /home/stefan/2ls/cbmc/src/util/prefix.h \
+ /home/stefan/2ls/cbmc/src/util/suffix.h \
+ /home/stefan/2ls/cbmc/src/util/cprover_prefix.h \
+ /home/stefan/2ls/cbmc/src/util/std_expr.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/dstring.h \
+ /home/stefan/2ls/cbmc/src/util/string_container.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/util/string_hash.h \
+ /home/stefan/2ls/cbmc/src/util/irep_ids.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/big-int/bigint.hh \
+ /home/stefan/2ls/cbmc/src/util/pointer_predicates.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h \
+ /home/stefan/2ls/cbmc/src/util/pointer_offset_size.h \
+ /home/stefan/2ls/cbmc/src/util/arith_tools.h \
+ /home/stefan/2ls/cbmc/src/util/byte_operators.h \
+ /home/stefan/2ls/cbmc/src/util/base_type.h \
+ /home/stefan/2ls/cbmc/src/util/union_find.h \
+ /home/stefan/2ls/cbmc/src/util/numbering.h \
+ /home/stefan/2ls/cbmc/src/util/expr_util.h \
+ /home/stefan/2ls/cbmc/src/util/simplify_expr.h \
+ /home/stefan/2ls/cbmc/src/ansi-c/c_types.h \
+ /home/stefan/2ls/cbmc/src/util/type.h ssa_dereference.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h ssa_value_set.h \
+ /home/stefan/2ls/cbmc/src/analyses/ai.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_model.h \
+ /home/stefan/2ls/cbmc/src/util/symbol_table.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program.h \
+ /home/stefan/2ls/cbmc/src/util/std_code.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_util.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h ssa_object.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h \
+ address_canonizer.h /home/stefan/2ls/cbmc/src/util/expr.h
+
+/home/stefan/2ls/cbmc/src/util/prefix.h:
+
+/home/stefan/2ls/cbmc/src/util/suffix.h:
+
+/home/stefan/2ls/cbmc/src/util/cprover_prefix.h:
+
+/home/stefan/2ls/cbmc/src/util/std_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/dstring.h:
+
+/home/stefan/2ls/cbmc/src/util/string_container.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/util/string_hash.h:
+
+/home/stefan/2ls/cbmc/src/util/irep_ids.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/big-int/bigint.hh:
+
+/home/stefan/2ls/cbmc/src/util/pointer_predicates.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
+
+/home/stefan/2ls/cbmc/src/util/pointer_offset_size.h:
+
+/home/stefan/2ls/cbmc/src/util/arith_tools.h:
+
+/home/stefan/2ls/cbmc/src/util/byte_operators.h:
+
+/home/stefan/2ls/cbmc/src/util/base_type.h:
+
+/home/stefan/2ls/cbmc/src/util/union_find.h:
+
+/home/stefan/2ls/cbmc/src/util/numbering.h:
+
+/home/stefan/2ls/cbmc/src/util/expr_util.h:
+
+/home/stefan/2ls/cbmc/src/util/simplify_expr.h:
+
+/home/stefan/2ls/cbmc/src/ansi-c/c_types.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+ssa_dereference.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
+
+ssa_value_set.h:
+
+/home/stefan/2ls/cbmc/src/analyses/ai.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_model.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol_table.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program.h:
+
+/home/stefan/2ls/cbmc/src/util/std_code.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_util.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
+
+ssa_object.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h:
+
+address_canonizer.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
---
+++ src/summarizer/array_abstraction.d
@@ -0,0 +1,129 @@
+array_abstraction.o: array_abstraction.cpp \
+ /home/stefan/2ls/cbmc/src/util/std_expr.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/dstring.h \
+ /home/stefan/2ls/cbmc/src/util/string_container.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/util/string_hash.h \
+ /home/stefan/2ls/cbmc/src/util/irep_ids.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/big-int/bigint.hh \
+ /home/stefan/2ls/cbmc/src/util/std_code.h \
+ /home/stefan/2ls/cbmc/src/util/expr_util.h \
+ /home/stefan/2ls/cbmc/src/util/message.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/arith_tools.h \
+ /home/stefan/2ls/cbmc/src/util/i2string.h \
+ /home/stefan/2ls/cbmc/src/util/prefix.h \
+ /home/stefan/2ls/cbmc/src/util/cprover_prefix.h \
+ /home/stefan/2ls/cbmc/src/util/type_eq.h \
+ /home/stefan/2ls/cbmc/src/ansi-c/c_types.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/pointer_arithmetic.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h array_abstraction.h \
+ /home/stefan/2ls/cbmc/src/util/symbol_table.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h \
+ /home/stefan/2ls/cbmc/src/util/message_stream.h \
+ /home/stefan/2ls/cbmc/src/util/message.h \
+ /home/stefan/2ls/cbmc/src/util/config.h \
+ /home/stefan/2ls/cbmc/src/util/ieee_float.h \
+ /home/stefan/2ls/cbmc/src/util/format_spec.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_util.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h
+
+/home/stefan/2ls/cbmc/src/util/std_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/dstring.h:
+
+/home/stefan/2ls/cbmc/src/util/string_container.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/util/string_hash.h:
+
+/home/stefan/2ls/cbmc/src/util/irep_ids.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/big-int/bigint.hh:
+
+/home/stefan/2ls/cbmc/src/util/std_code.h:
+
+/home/stefan/2ls/cbmc/src/util/expr_util.h:
+
+/home/stefan/2ls/cbmc/src/util/message.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/arith_tools.h:
+
+/home/stefan/2ls/cbmc/src/util/i2string.h:
+
+/home/stefan/2ls/cbmc/src/util/prefix.h:
+
+/home/stefan/2ls/cbmc/src/util/cprover_prefix.h:
+
+/home/stefan/2ls/cbmc/src/util/type_eq.h:
+
+/home/stefan/2ls/cbmc/src/ansi-c/c_types.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/pointer_arithmetic.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+array_abstraction.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol_table.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
+
+/home/stefan/2ls/cbmc/src/util/message_stream.h:
+
+/home/stefan/2ls/cbmc/src/util/message.h:
+
+/home/stefan/2ls/cbmc/src/util/config.h:
+
+/home/stefan/2ls/cbmc/src/util/ieee_float.h:
+
+/home/stefan/2ls/cbmc/src/util/format_spec.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_util.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
