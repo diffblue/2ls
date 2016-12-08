4b0c06f4fb39833aaf2c319636fa4740e90abcfc
Stefan <steffan711@gmail.com>
2016-12-08 15:06:02 +0100

    first commit

---
+++ src/domains/domain.d
@@ -0,0 +1,56 @@
+domain.o: domain.cpp domain.h /home/stefan/2ls/cbmc/src/util/std_expr.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/dstring.h \
+ /home/stefan/2ls/cbmc/src/util/string_container.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/util/string_hash.h \
+ /home/stefan/2ls/cbmc/src/util/irep_ids.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/big-int/bigint.hh \
+ /home/stefan/2ls/cbmc/src/util/i2string.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_util.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/replace_expr.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h
+
+domain.h:
+
+/home/stefan/2ls/cbmc/src/util/std_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/dstring.h:
+
+/home/stefan/2ls/cbmc/src/util/string_container.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/util/string_hash.h:
+
+/home/stefan/2ls/cbmc/src/util/irep_ids.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/big-int/bigint.hh:
+
+/home/stefan/2ls/cbmc/src/util/i2string.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_util.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/replace_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
