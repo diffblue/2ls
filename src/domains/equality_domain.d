d29fcf60e599ca35256751bbb3161613ca6e1b97
Stefan <steffan711@gmail.com>
2016-12-08 15:06:02 +0100

    first commit

---
+++ src/domains/equality_domain.d
@@ -0,0 +1,94 @@
+equality_domain.o: equality_domain.cpp equality_domain.h \
+ /home/stefan/2ls/cbmc/src/util/std_expr.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/dstring.h \
+ /home/stefan/2ls/cbmc/src/util/string_container.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/util/string_hash.h \
+ /home/stefan/2ls/cbmc/src/util/irep_ids.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/big-int/bigint.hh \
+ /home/stefan/2ls/cbmc/src/util/union_find.h \
+ /home/stefan/2ls/cbmc/src/util/numbering.h domain.h \
+ /home/stefan/2ls/cbmc/src/util/i2string.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_util.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/replace_expr.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h util.h \
+ /home/stefan/2ls/cbmc/src/util/arith_tools.h \
+ /home/stefan/2ls/cbmc/src/util/ieee_float.h \
+ /home/stefan/2ls/cbmc/src/util/format_spec.h \
+ /home/stefan/2ls/cbmc/src/util/find_symbols.h \
+ /home/stefan/2ls/cbmc/src/util/simplify_expr.h \
+ /home/stefan/2ls/cbmc/src/langapi/languages.h \
+ /home/stefan/2ls/cbmc/src/util/language.h \
+ /home/stefan/2ls/cbmc/src/util/message.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h
+
+equality_domain.h:
+
+/home/stefan/2ls/cbmc/src/util/std_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/dstring.h:
+
+/home/stefan/2ls/cbmc/src/util/string_container.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/util/string_hash.h:
+
+/home/stefan/2ls/cbmc/src/util/irep_ids.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/big-int/bigint.hh:
+
+/home/stefan/2ls/cbmc/src/util/union_find.h:
+
+/home/stefan/2ls/cbmc/src/util/numbering.h:
+
+domain.h:
+
+/home/stefan/2ls/cbmc/src/util/i2string.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_util.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/replace_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
+
+util.h:
+
+/home/stefan/2ls/cbmc/src/util/arith_tools.h:
+
+/home/stefan/2ls/cbmc/src/util/ieee_float.h:
+
+/home/stefan/2ls/cbmc/src/util/format_spec.h:
+
+/home/stefan/2ls/cbmc/src/util/find_symbols.h:
+
+/home/stefan/2ls/cbmc/src/util/simplify_expr.h:
+
+/home/stefan/2ls/cbmc/src/langapi/languages.h:
+
+/home/stefan/2ls/cbmc/src/util/language.h:
+
+/home/stefan/2ls/cbmc/src/util/message.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
