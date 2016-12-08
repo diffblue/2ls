72fefd7575bc24c7420b98ab5ac76548b5a73771
Stefan <steffan711@gmail.com>
2016-12-08 15:06:02 +0100

    first commit

---
+++ src/domains/template_generator_summary.d
@@ -0,0 +1,278 @@
+template_generator_summary.o: template_generator_summary.cpp \
+ template_generator_summary.h template_generator_base.h \
+ /home/stefan/2ls/cbmc/src/util/options.h \
+ /home/stefan/2ls/cbmc/src/util/replace_expr.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/util/dstring.h \
+ /home/stefan/2ls/cbmc/src/util/string_container.h \
+ /home/stefan/2ls/cbmc/src/util/string_hash.h \
+ /home/stefan/2ls/cbmc/src/util/irep_ids.h ../ssa/local_ssa.h \
+ /home/stefan/2ls/cbmc/src/util/std_expr.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/big-int/bigint.hh \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program.h \
+ /home/stefan/2ls/cbmc/src/util/std_code.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h \
+ /home/stefan/2ls/cbmc/src/util/namespace.h \
+ /home/stefan/2ls/cbmc/src/util/symbol_table.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_util.h \
+ /home/stefan/2ls/cbmc/src/util/irep.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h \
+ /home/stefan/2ls/cbmc/src/util/std_types.h \
+ /home/stefan/2ls/cbmc/src/util/symbol.h \
+ ../ssa/../domains/incremental_solver.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/bv_pointers.h \
+ /home/stefan/2ls/cbmc/src/util/hash_cont.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/boolbv.h \
+ /home/stefan/2ls/cbmc/src/util/mp_arith.h \
+ /home/stefan/2ls/cbmc/src/util/expr.h \
+ /home/stefan/2ls/cbmc/src/util/byte_operators.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/bv_utils.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/prop.h \
+ /home/stefan/2ls/cbmc/src/util/message.h \
+ /home/stefan/2ls/cbmc/src/util/source_location.h \
+ /home/stefan/2ls/cbmc/src/util/threeval.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/prop_assignment.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/literal.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_width.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_map.h \
+ /home/stefan/2ls/cbmc/src/util/type.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_type.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/arrays.h \
+ /home/stefan/2ls/cbmc/src/util/union_find.h \
+ /home/stefan/2ls/cbmc/src/util/numbering.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/equality.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/prop_conv.h \
+ /home/stefan/2ls/cbmc/src/util/decision_procedure.h \
+ /home/stefan/2ls/cbmc/src/util/message.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/literal_expr.h \
+ /home/stefan/2ls/cbmc/src/solvers/prop/prop.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/functions.h \
+ /home/stefan/2ls/cbmc/src/solvers/flattening/pointer_logic.h \
+ /home/stefan/2ls/cbmc/src/util/numbering.h \
+ /home/stefan/2ls/cbmc/src/solvers/refinement/bv_refinement.h \
+ /home/stefan/2ls/cbmc/src/langapi/language_ui.h \
+ /home/stefan/2ls/cbmc/src/util/language_file.h \
+ /home/stefan/2ls/cbmc/src/util/ui_message.h \
+ /home/stefan/2ls/cbmc/src/solvers/sat/satcheck.h \
+ /home/stefan/2ls/cbmc/src/solvers/sat/satcheck_minisat2.h \
+ /home/stefan/2ls/cbmc/src/solvers/sat/cnf.h ../ssa/../domains/domain.h \
+ /home/stefan/2ls/cbmc/src/util/i2string.h ../ssa/../domains/util.h \
+ /home/stefan/2ls/cbmc/src/util/arith_tools.h \
+ /home/stefan/2ls/cbmc/src/util/ieee_float.h \
+ /home/stefan/2ls/cbmc/src/util/format_spec.h ../ssa/ssa_domain.h \
+ /home/stefan/2ls/cbmc/src/analyses/ai.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_model.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h \
+ ../ssa/assignments.h ../ssa/ssa_object.h ../ssa/ssa_value_set.h \
+ ../ssa/guard_map.h \
+ /home/stefan/2ls/cbmc/src/goto-programs/goto_program.h \
+ ../ssa/ssa_unwinder.h ../ssa/unwindable_local_ssa.h ../ssa/local_ssa.h \
+ ../ssa/../summarizer/ssa_db.h ../ssa/../summarizer/../ssa/local_ssa.h \
+ ../ssa/../summarizer/../ssa/unwindable_local_ssa.h \
+ ../ssa/../summarizer/../domains/incremental_solver.h \
+ strategy_solver_base.h domain.h util.h ../domains/incremental_solver.h \
+ equality_domain.h tpolyhedra_domain.h \
+ /home/stefan/2ls/cbmc/src/util/find_symbols.h \
+ /home/stefan/2ls/cbmc/src/util/simplify_expr.h
+
+template_generator_summary.h:
+
+template_generator_base.h:
+
+/home/stefan/2ls/cbmc/src/util/options.h:
+
+/home/stefan/2ls/cbmc/src/util/replace_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/util/dstring.h:
+
+/home/stefan/2ls/cbmc/src/util/string_container.h:
+
+/home/stefan/2ls/cbmc/src/util/string_hash.h:
+
+/home/stefan/2ls/cbmc/src/util/irep_ids.h:
+
+../ssa/local_ssa.h:
+
+/home/stefan/2ls/cbmc/src/util/std_expr.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/big-int/bigint.hh:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program.h:
+
+/home/stefan/2ls/cbmc/src/util/std_code.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program_template.h:
+
+/home/stefan/2ls/cbmc/src/util/namespace.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol_table.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_util.h:
+
+/home/stefan/2ls/cbmc/src/util/irep.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions_template.h:
+
+/home/stefan/2ls/cbmc/src/util/std_types.h:
+
+/home/stefan/2ls/cbmc/src/util/symbol.h:
+
+../ssa/../domains/incremental_solver.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/bv_pointers.h:
+
+/home/stefan/2ls/cbmc/src/util/hash_cont.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/boolbv.h:
+
+/home/stefan/2ls/cbmc/src/util/mp_arith.h:
+
+/home/stefan/2ls/cbmc/src/util/expr.h:
+
+/home/stefan/2ls/cbmc/src/util/byte_operators.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/bv_utils.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/prop.h:
+
+/home/stefan/2ls/cbmc/src/util/message.h:
+
+/home/stefan/2ls/cbmc/src/util/source_location.h:
+
+/home/stefan/2ls/cbmc/src/util/threeval.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/prop_assignment.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/literal.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_width.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_map.h:
+
+/home/stefan/2ls/cbmc/src/util/type.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/boolbv_type.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/arrays.h:
+
+/home/stefan/2ls/cbmc/src/util/union_find.h:
+
+/home/stefan/2ls/cbmc/src/util/numbering.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/equality.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/prop_conv.h:
+
+/home/stefan/2ls/cbmc/src/util/decision_procedure.h:
+
+/home/stefan/2ls/cbmc/src/util/message.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/literal_expr.h:
+
+/home/stefan/2ls/cbmc/src/solvers/prop/prop.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/functions.h:
+
+/home/stefan/2ls/cbmc/src/solvers/flattening/pointer_logic.h:
+
+/home/stefan/2ls/cbmc/src/util/numbering.h:
+
+/home/stefan/2ls/cbmc/src/solvers/refinement/bv_refinement.h:
+
+/home/stefan/2ls/cbmc/src/langapi/language_ui.h:
+
+/home/stefan/2ls/cbmc/src/util/language_file.h:
+
+/home/stefan/2ls/cbmc/src/util/ui_message.h:
+
+/home/stefan/2ls/cbmc/src/solvers/sat/satcheck.h:
+
+/home/stefan/2ls/cbmc/src/solvers/sat/satcheck_minisat2.h:
+
+/home/stefan/2ls/cbmc/src/solvers/sat/cnf.h:
+
+../ssa/../domains/domain.h:
+
+/home/stefan/2ls/cbmc/src/util/i2string.h:
+
+../ssa/../domains/util.h:
+
+/home/stefan/2ls/cbmc/src/util/arith_tools.h:
+
+/home/stefan/2ls/cbmc/src/util/ieee_float.h:
+
+/home/stefan/2ls/cbmc/src/util/format_spec.h:
+
+../ssa/ssa_domain.h:
+
+/home/stefan/2ls/cbmc/src/analyses/ai.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_model.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_functions.h:
+
+../ssa/assignments.h:
+
+../ssa/ssa_object.h:
+
+../ssa/ssa_value_set.h:
+
+../ssa/guard_map.h:
+
+/home/stefan/2ls/cbmc/src/goto-programs/goto_program.h:
+
+../ssa/ssa_unwinder.h:
+
+../ssa/unwindable_local_ssa.h:
+
+../ssa/local_ssa.h:
+
+../ssa/../summarizer/ssa_db.h:
+
+../ssa/../summarizer/../ssa/local_ssa.h:
+
+../ssa/../summarizer/../ssa/unwindable_local_ssa.h:
+
+../ssa/../summarizer/../domains/incremental_solver.h:
+
+strategy_solver_base.h:
+
+domain.h:
+
+util.h:
+
+../domains/incremental_solver.h:
+
+equality_domain.h:
+
+tpolyhedra_domain.h:
+
+/home/stefan/2ls/cbmc/src/util/find_symbols.h:
+
+/home/stefan/2ls/cbmc/src/util/simplify_expr.h:
