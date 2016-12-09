ba49dcf5fe7d6e0918bdbc326c82168b3d31e543
Peter Schrammel <p.schrammel@sussex.ac.uk>
2016-12-09 22:40:43 +0000

    Graphml correctness witness extension stub

---
+++ src/summarizer/graphml_witness_ext.h
@@ -0,0 +1,21 @@
+/*******************************************************************\
+
+Module: CFA for GraphML output
+
+Author: Peter Schrammel
+
+\*******************************************************************/
+
+#ifndef CPROVER_2LS_SSA_SSA_GRAPHML_CFA_H
+#define CPROVER_2LS_SSA_SSA_GRAPHML_CFA_H
+
+#include <util/std_expr.h>
+
+#include <goto-programs/goto_functions.h>
+
+class ssa_graphml_cfat
+{
+public:
+  
+protected:
+};
