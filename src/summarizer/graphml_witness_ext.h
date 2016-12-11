1455ffb5192a93ed333cbb04c066cd6808b25059
Peter Schrammel <p.schrammel@sussex.ac.uk>
2016-12-11 13:28:37 +0000

    Graphml correctness witnesses

--- src/summarizer/graphml_witness_ext.h
+++ src/summarizer/graphml_witness_ext.h
@@ -1,21 +1,47 @@
 /*******************************************************************\
 
-Module: CFA for GraphML output
+Module: SSA CFA extension for GraphML output
 
 Author: Peter Schrammel
 
 \*******************************************************************/
 
-#ifndef CPROVER_2LS_SSA_SSA_GRAPHML_CFA_H
-#define CPROVER_2LS_SSA_SSA_GRAPHML_CFA_H
+#ifndef CPROVER_2LS_SUMMARIZER_GRAPHML_WITNESS_EXT_H
+#define CPROVER_2LS_SUMMARIZER_GRAPHML_WITNESS_EXT_H
 
 #include <util/std_expr.h>
 
-#include <goto-programs/goto_functions.h>
+#include <goto-programs/graphml_witness.h>
 
-class ssa_graphml_cfat
+#include "dynamic_cfg.h"
+#include "summary_checker_base.h"
+
+
+class graphml_witness_extt:public graphml_witnesst
 {
 public:
+  explicit graphml_witness_extt(const namespacet &ns):
+    graphml_witnesst(ns) {}
+
+  // correctness witness
+  void operator()(const summary_checker_baset &summary_checker);
   
 protected:
+
+  graphmlt::node_indext add_node(
+    std::map<unsigned, unsigned> &loc_to_node,
+    goto_programt::const_targett it
+    );
+
+  void add_edge(
+    const graphmlt::node_indext &from,
+    const dynamic_cfg_nodet &from_cfg_node,
+    const graphmlt::node_indext &to,
+    const dynamic_cfg_nodet &to_cfg_node);
+
+  graphmlt::node_indext add_node(
+    const dynamic_cfg_nodet &cfg_node);
+
 };
+
+#endif // CPROVER_2LS_SUMMARIZER_GRAPHML_WITNESS_EXT_H
