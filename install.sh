2d0ecf6f4aec3cd07cd22820ac0887c26e47006c
Peter Schrammel <p.schrammel@sussex.ac.uk>
2017-01-06 23:56:16 +0100

    Fix compilation on travis

--- install.sh
+++ install.sh
@@ -1,13 +1,31 @@
-git clone https://github.com/peterschrammel/cbmc
+CBMC_REPO=https://github.com/peterschrammel/cbmc
+CBMC_VERSION=d95e7da28018fd315b04a1201d5b7cfe8195cbc6
+
+if [ "$1" != "" ]
+then
+  COMPILER=$1
+fi
+
+git clone $CBMC_REPO
 cd cbmc
 CBMC=`pwd`
-git checkout d95e7da28018fd315b04a1201d5b7cfe8195cbc6
-cd src
-make minisat2-download
-make
-cd ../../src
+git checkout $CBMC_VERSION
+make -C src minisat2-download
+if [ "$COMPILER" != ""]
+then
+  make -C src CXX=$COMPILER
+else
+  make -C src
+fi
+
+cd ../src
 cp config.inc.template config.inc
 sed -i.bak "s#CBMC = ~/my-cbmc#CBMC = $CBMC#g" config.inc
-make
+if [ "$COMPILER" != ""]
+then
+  make CXX=$COMPILER
+else
+  make
+fi
 cd ..
-echo "The executable is src/summarizer/2ls"
+echo "The executable is src/2ls/2ls"
