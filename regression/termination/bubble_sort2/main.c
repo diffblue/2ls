2c49d5723f7d42fe1a6a82513f5e1096f2237257
Peter Schrammel <p.schrammel@sussex.ac.uk>
2017-01-31 15:58:26 +0000

    Bubble sort termination test

---
+++ regression/termination/bubble_sort2/main.c
@@ -0,0 +1,20 @@
+void main()
+{
+  int n;
+  __CPROVER_assume(2<=n && n<=100);
+  int array[n];
+  int c, d, swap;
+  
+  for (c = 0 ; c < ( n - 1 ); c++)
+  {
+    for (d = 0 ; d < n - c - 1; d++)
+    {
+      if (array[d] > array[d+1])
+      {
+        swap       = array[d];
+        array[d]   = array[d+1];
+        array[d+1] = swap;
+      }
+    }
+  }
+}
