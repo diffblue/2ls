9e6fb86fc1f293ed5bb54cf3a776dbf634c3c396
Viktor Malik <viktor.malik@gmail.com>
2017-04-06 17:23:55 +0200

    SSA heap analysis: new interprocedural analysis. Interprocedural analysis that determines which dynamic objects are created and modified within a function. TODO: switch from static_analysist to ait.

---
+++ src/ssa/ssa_heap_domain.h
@@ -0,0 +1,74 @@
+/**
+ *  Viktor Malik, 2/28/17 (c).
+ */
+#ifndef INC_2LS_SSA_HEAP_DOMAIN_H
+#define INC_2LS_SSA_HEAP_DOMAIN_H
+
+
+#include <analyses/static_analysis.h>
+
+class ssa_heap_domaint : public domain_baset
+{
+ public:
+  virtual void transform(const namespacet &ns, locationt from, locationt to) override;
+  bool merge(const ssa_heap_domaint &, locationt);
+
+  irep_idt function;
+
+  typedef std::set<exprt> objectst;
+  std::map<exprt, objectst> value_map;
+
+  const std::set<symbol_exprt> value(const exprt &expr) const;
+
+  class function_infot
+  {
+   public:
+    std::map<symbol_exprt, std::set<exprt> > new_objects;
+    std::set<exprt> modified_objects;
+    std::vector<irep_idt> params;
+
+    const exprt corresponding_expr(const exprt &expr,
+                                   const code_function_callt::argumentst &arguments,
+                                   unsigned deref_level) const;
+
+
+   protected:
+    const exprt apply_deref(const exprt &expr, unsigned level) const;
+  };
+
+  std::map<irep_idt, function_infot> function_map;
+
+  const std::list<symbol_exprt> new_objects() const;
+  const std::list<symbol_exprt> new_objects(const irep_idt &fname) const;
+  const std::list<symbol_exprt> new_caller_objects(const irep_idt &fname, locationt loc) const;
+
+  const std::set<exprt> modified_objects(const irep_idt &fname) const;
+ protected:
+
+  void assign_lhs_rec(const exprt &lhs, const exprt &rhs, const namespacet &ns);
+
+  void assign_rhs(const exprt &rhs, const irep_idt &function, objectst &objects,
+                    const namespacet &ns);
+
+  bool is_function_output(const exprt &expr, const irep_idt &function,
+                            const namespacet &ns, bool in_deref);
+
+  void rename_to_caller(symbol_exprt &object, locationt loc, unsigned &index) const;
+
+  void update_modified(const exprt &expr, const namespacet &ns);
+};
+
+class ssa_heap_analysist : public static_analysist<ssa_heap_domaint>
+{
+ public:
+  explicit ssa_heap_analysist(const namespacet &_ns)
+      : static_analysist(_ns) {}
+
+  virtual void initialize(const goto_functionst &goto_functions) override;
+
+ protected:
+  void init_ptr_param(const exprt &expr, ssa_heap_domaint &f_entry);
+};
+
+
+#endif //INC_2LS_SSA_HEAP_DOMAIN_H
