596ea8658c1287865f80a910d7be9419e3969f7b
Viktor Malik <viktor.malik@gmail.com>
2017-04-07 08:32:47 +0200

    SSA inliner: remove new_arg_out function. We don't need it anymore since output heap objects are determined from SSA heap analysis.

--- src/ssa/ssa_inliner.h
+++ src/ssa/ssa_inliner.h
@@ -194,11 +194,6 @@ protected:
     const local_SSAt &SSA,
     local_SSAt::locationt loc);
 
-  const exprt new_pointed_arg(
-    const exprt &arg,
-    const typet &pointed_type,
-    const std::list<exprt> &args_out);
-
   static bool contains_iterator(const std::list<exprt> &params);
 };
 
