7ae7415652986c29128f74472d2a4198cfc8dd99
Viktor Malik <viktor.malik@gmail.com>
2017-07-03 08:08:27 +0200

    Heap-interval domain: remove abstract value from the domain (it is not used).

--- src/domains/heap_interval_domain.h
+++ src/domains/heap_interval_domain.h
@@ -15,8 +15,6 @@ class heap_interval_domaint : public domaint
   heap_domaint heap_domain;
   tpolyhedra_domaint interval_domain;
 
-  heap_domaint::heap_valuet heap_value;
-
   heap_interval_domaint(
     unsigned int _domain_number,
     replace_mapt &_renaming_map,
