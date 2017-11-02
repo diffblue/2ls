ea1062d4cf95134217390b54f1cbd20a4d22e5f1
Martin Hruska <hruskamartin25@gmail.com>
2017-11-02 14:36:35 +0100

    recursive concretisation of rhs

--- src/ssa/local_ssa.cpp
+++ src/ssa/local_ssa.cpp
@@ -2030,6 +2030,7 @@ exprt local_SSAt::concretise_symbolic_deref_rhs(
     const exprt deref_rhs=dereference(rhs, loc);
     const exprt symbolic_deref_rhs=symbolic_dereference(rhs, ns);
     ssa_objectt rhs_object(symbolic_deref_rhs, ns);
+
     if(deref_rhs.get_bool("#heap_access") && rhs_object)
     {
       const exprt pointer=get_pointer(
@@ -2052,7 +2053,12 @@ exprt local_SSAt::concretise_symbolic_deref_rhs(
         return symbolic_deref_rhs;
       }
     }
+    else
+    {
+      forall_operands(it, rhs)
+        concretise_symbolic_deref_rhs(*it, ns, loc);
+    }
 
-    return (all_symbolic_deref_defined(symbolic_deref_rhs, ns, loc))
-           ? symbolic_deref_rhs : deref_rhs;
+    return all_symbolic_deref_defined(symbolic_deref_rhs, ns, loc)
+          ? symbolic_deref_rhs : deref_rhs;
 }
