18552d41b9fe4a9cf3ef099d179235a81c2ede3a
Peter Schrammel <peter.schrammel@diffblue.com>
2017-11-12 11:04:43 +0000

    Fix nontermination tests

---
+++ regression/nontermination/nontermloop2/main.c
@@ -0,0 +1,10 @@
+void main()
+{
+  int x = 1;
+
+  while(x!=0)
+  {
+    if(x==1) x++;
+    else x--;
+  }
+}
---
+++ regression/nontermination/nontermloop3/main.c
@@ -0,0 +1,11 @@
+void main()
+{
+  char x;
+  //  __CPROVER_assume(-3<=x && x<=3);
+
+  while(1)
+  {
+    //    x = (x-1)%4;
+    x--;
+  }
+}
