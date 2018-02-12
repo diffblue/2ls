8e084da113546ecbb1a693132c0b5282998e15e6
Viktor Malik <viktor.malik@gmail.com>
2018-02-12 08:42:09 +0100

    Heap-interval domain: improve domain combination
    If heap part is improved, restrict the interval domain to the symbolic path
    found by the heap part. Also add the current state of the invariant of one
    part as a context to solving of the other part.

--- src/domains/strategy_solver_heap_interval.cpp
+++ src/domains/strategy_solver_heap_interval.cpp
@@ -16,9 +16,10 @@ Function: strategy_solver_heap_intervalt::iterate
 
  Outputs:
 
- Purpose: Since template rows for the shape part and the interval part
-          are disjoint, we simply call iterate method for each part
-          separately
+ Purpose: Run iteration of each solver separately, but every time
+          in the context of the current invariant found by the other
+          solver. The interval solving is also restricted to
+          a symbolic path found by the heap solver.
 
 \*******************************************************************/
 
@@ -28,8 +29,30 @@ bool strategy_solver_heap_intervalt::iterate(
   heap_interval_domaint::heap_interval_valuet &inv=
     static_cast<heap_interval_domaint::heap_interval_valuet &>(_inv);
 
+  // Run one iteration of heap solver in the context of invariant from
+  // the interval solver
+  solver.new_context();
+  solver << heap_interval_domain.interval_domain.to_pre_constraints(
+    inv.interval_value);
   bool heap_improved=heap_solver.iterate(inv.heap_value);
+  solver.pop_context();
+
+  if(heap_improved)
+  {
+    // If heap part was improved, restrict interval part to found symbolic path
+    symbolic_path=heap_solver.symbolic_path;
+    heap_interval_domain.interval_domain.restrict_to_sympath(symbolic_path);
+  }
+
+  // Run one interation of interval solver in the context of invariant from
+  // the heap solver
+  solver.new_context();
+  solver << heap_interval_domain.heap_domain.to_pre_constraints(inv.heap_value);
   bool interval_improved=interval_solver.iterate(inv.interval_value);
+  solver.pop_context();
+
+  if(heap_improved)
+    heap_interval_domain.interval_domain.undo_restriction();
 
   return heap_improved || interval_improved;
 }
