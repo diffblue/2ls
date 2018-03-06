8705014d52be94d96ec0bc45b0774788b3837067
Viktor Malik <viktor.malik@gmail.com>
2018-03-06 09:06:58 +0100

    Add tests for memory safety
    Tests check pointer safety for linked lists.

---
+++ regression/memsafety/simple_false/main.c
@@ -0,0 +1,39 @@
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+
+extern int __VERIFIER_nondet_int();
+
+#include <stdlib.h>
+
+void exit(int s) {
+	_EXIT: goto _EXIT;
+}
+
+typedef struct node {
+  int h;
+  struct node *n;
+} *List;
+
+int main() {
+  /* Build a list of the form 1->...->1->0 */
+  List a = (List) malloc(sizeof(struct node));
+  if (a == 0) exit(1);
+  a->n = 0;
+  List t;
+  List p = a;
+  while (__VERIFIER_nondet_int()) {
+    t = (List) malloc(sizeof(struct node));
+    if (t == 0) exit(1);
+    t->n = 0;
+    p->n = t;
+    p = p->n;
+  }
+  p->n = a;
+  p = a;
+  while (p!=0) {
+    t = p->n;
+    free(p);
+    p = t;
+  }
+  return 0;
+}
+
