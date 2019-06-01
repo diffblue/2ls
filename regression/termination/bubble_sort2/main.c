5b29c0f1dedb06d2dcbbdc113e43ace3e0c74673
Peter Schrammel <peter.schrammel@diffblue.com>
2019-06-01 15:49:07 +0100

    Updated termination example

--- regression/termination/bubble_sort2/main.c
+++ regression/termination/bubble_sort2/main.c
@@ -1,19 +1,17 @@
-void main()
+void bubble_sort(int *array, int size)
 {
-  int n;
-  __CPROVER_assume(2<=n && n<=100);
-  int array[n];
+  __CPROVER_assume(size >= 0);
   int c, d, swap;
 
-  for (c = 0 ; c < ( n - 1 ); c++)
+  for (c = 0; c < size - 1; c++)
   {
-    for (d = 0 ; d < n - c - 1; d++)
+    for (d = 0; d < size - c - 1; d++)
     {
-      if (array[d] > array[d+1])
+      if (array[d] > array[d + 1])
       {
         swap       = array[d];
-        array[d]   = array[d+1];
-        array[d+1] = swap;
+        array[d]   = array[d + 1];
+        array[d + 1] = swap;
       }
     }
   }
